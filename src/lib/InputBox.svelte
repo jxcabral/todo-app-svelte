dea5f6ffacd7b5bb63ff09c84f86c53896edb9f8
Juan Cruz Cabral <cabraljcruz@gmail.com>
2022-03-12 23:26:14 -0300

    feat: input component for creating todo

---
+++ src/lib/InputBox.svelte
@@ -0,0 +1,37 @@
+<script>
+  import { todos } from "../store";
+  
+  let value = "";
+
+  const addTodo = () => {
+    const newTodo = { value, done: false }
+    
+    $todos = [newTodo,...$todos]
+    value = ''
+  }
+
+  const addOnEnter = (evt) => {
+    if(evt.key === 'Enter') {
+      evt.preventDefault()
+      addTodo()
+    }
+  }
+  
+</script>
+
+<div class="box mb-6">
+  <textarea id="todo-text" bind:value class="textarea has-fixed-size" placeholder="My awesome todo" rows="2" on:keydown={addOnEnter} />
+  <button disabled={value === ""} on:click={addTodo} class="button is-success is-medium is-fullwidth mt-3">Create</button>
+</div>
+
+<style>
+  div {
+    width: 100%;
+  }
+
+  /* @media screen and (max-width: 414px) {
+    textarea {
+      width: 20rem;
+    }
+  } */
+</style>
