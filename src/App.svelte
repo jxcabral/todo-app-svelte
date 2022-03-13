0f357b7e6b4b3da5398a6b1086dc06062275fcfe
Juan Cruz Cabral <cabraljcruz@gmail.com>
2022-03-12 23:29:11 -0300

    feat: core app

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+  import InputBox from "./lib/InputBox.svelte";
+  import TodoList from "./lib/TodoList.svelte";
+</script>
+
+<main class="has-background-info is-flex is-flex-direction-column is-align-items-center">
+  <h1 class="is-size-1 has-text-weight-bold has-text-white my-3">TODO APP</h1>
+  <InputBox />
+  <TodoList />
+</main>
+
+<style>
+  main {
+    height: 100vh;
+    padding: 1.5rem;
+  }
+
+  @media (min-width: 1024px) {
+    main {
+      padding: 0 16rem;
+    }
+  }
+</style>
